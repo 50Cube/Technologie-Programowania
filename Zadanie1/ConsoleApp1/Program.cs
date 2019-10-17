2ede8320448c4e2bb0bd867708e20f8a90d3fb73
Jakub Bogdan <kubab1998@o2.pl>
2019-10-17 21:51:54 +0200

    Utworzone pierwsze klasy

---
+++ Zadanie1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zadanie1;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Wykaz osoba1 = new Wykaz(2137, "Gabor", "Nowag");
+            Console.WriteLine(osoba1.Id);
+            osoba1.Id = 997;
+            Console.Write(osoba1.Id);
+            Console.Read();
+        }
+    }
+}
---
+++ Zadanie1/Zadanie1/Wykaz.cs
@@ -0,0 +1,35 @@
+﻿namespace Zadanie1
+{
+    public class Wykaz
+    {
+        private int id;
+        private string imie;
+        private string nazwisko;
+
+        public Wykaz(int id, string imie, string nazwisko)
+        {
+            this.id = id;
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+        }
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Imie
+        {
+            get { return imie; }
+            set { imie = value; }
+        }
+
+        public string Nazwisko
+        {
+            get { return nazwisko; }
+            set { nazwisko = value; }
+        }
+
+    }
+}
