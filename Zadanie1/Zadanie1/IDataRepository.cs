cfe01bd946c71b6d635299c0ee245e2b30954439
Gabriel Nowak <gabrielnowak002@gmail.com>
2019-10-27 17:49:41 +0100

    Interface IDataRepository

---
+++ Zadanie1/Zadanie1/IDataRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Zadanie1
+{
+    public interface IDataRepository
+    {
+        void AddKatalog(Katalog katalog);
+        void AddOpisStanu(OpisStanu opis);
+        void AddWykaz(Wykaz wykaz);
+        void DeleteKatalog(int id);
+        bool DeleteOpisStanu(Katalog katalog);
+        bool DeleteWykaz(Wykaz wykaz);
+        Dictionary<int, Katalog> GetAllKatalog();
+        List<OpisStanu> GetAllOpisStanu();
+        List<Wykaz> GetAllWykaz();
+        Katalog GetKatalog(int id);
+        OpisStanu GetOpisStanu(Katalog katalog);
+        Wykaz GetWykaz(int id);
+        void UpdateKatalog(int id, Autor autor, string tytul, int rok);
+        bool UpdateOpisStanu(Katalog katalog, int ilosc, double cena, DateTime data);
+        bool UpdateWykaz(int id, string imie, string nazwisko);
+    }
+}
