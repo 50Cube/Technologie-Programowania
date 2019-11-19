90d0cc712da849539e749a248a2a28374b900b15
Gabriel Nowak <gabrielnowak002@gmail.com>
2019-11-19 19:21:13 +0100

    Podstawy aplikacji konsolowej 2

--- Zadanie1/ConsoleApp/Program.cs
+++ Zadanie1/ConsoleApp/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,25 +12,45 @@ namespace Zadanie2
         static void Main(string[] args)
         {
             int wybor = 0;
-
-            GrafObiektow grafObiektow = new GrafObiektow();
             DataContext dataContext = new DataContext();
-            grafObiektow.Wypelnij(dataContext);
+            while (true)
+            {
+                do
+                {
+                    Console.WriteLine("Wybierz funkcje programu");
+                    Console.WriteLine("1 - stwórz testowy graf obiektów");
+                    Console.WriteLine("2 - zapisz dane do pliku");
+                    Console.WriteLine("3 - wczytaj dane z pliku");
+                    Console.WriteLine("4 - zakoncz program");
+                    try
+                    {
+                        wybor = int.Parse(Console.ReadLine());
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Nieprawidlowy format");
+                    }
+                    Console.WriteLine(wybor);
+                } while (wybor < 0 || wybor > 4);
 
-            Console.WriteLine("Wybierz funkcje programu");
-            Console.WriteLine("1 - zapisz dane do pliku");
-            Console.WriteLine("2 - wczytaj dane z pliku");
-            Console.WriteLine("3 - zakoncz program");
-            
-            OperacjeJSON.Zapisz(dataContext);
-            
-
-            DataContext dataContext2 = new DataContext();
-            OperacjeJSON.Wczytaj(dataContext2);
-
-            //OperacjeJSON.Zapisz(dataContext2);
-            Console.Read();
-
+                switch (wybor)
+                {
+                    case 1:
+                        GrafObiektow grafObiektow = new GrafObiektow();
+                        grafObiektow.Wypelnij(dataContext);
+                        break;
+                    case 2:
+                        OperacjeJSON.Zapisz(dataContext);
+                        break;
+                    case 3:
+                        dataContext = new DataContext();
+                        OperacjeJSON.Wczytaj(dataContext);
+                        break;
+                    case 4:
+                        System.Environment.Exit(1);
+                        break;
+                }
+            }
         }
     }
 }
