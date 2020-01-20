b37f0a17c57271cc49ce49d73f9488d22e078c09
50Cube <kubab1998@o2.pl>
2020-01-20 22:35:46 +0100

    Wyświetlanie listy produktów

--- Zadanie1/ViewModel/ViewModelClass.cs
+++ Zadanie1/ViewModel/ViewModelClass.cs
@@ -1,6 +1,8 @@
 ﻿using Model;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Windows.Input;
 
 namespace ViewModel
@@ -8,19 +10,47 @@ namespace ViewModel
     public class ViewModelClass : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+        private DataRepository DataRepository { get; set; }
+        private Product product;
+        private List<Product> products;
 
-        private Product Product;
-        private List<Product> Products;
+        public ViewModelClass()
+        {
+            DataRepository = new DataRepository();
+            Products = DataRepository.GetAll().ToList();
+            Product = new Product();
+        }
 
-        public DataRepository DataRepository { get; set; }
+        public Product Product
+        {
+            get => product;
+
+            set
+            {
+                product = value;
+                RaisePropertyChanged("Product");
+            }
+        }
+
+        public List<Product> Products
+        {
+            get => products;
+
+            set
+            {
+                products = value;
+                RaisePropertyChanged("Products");
+            }
+        }
+
 
         public ICommand AddProductCommand { get; set; }
         public ICommand UpdateProductCommand { get; set; }
         public ICommand DeleteProductCommand { get; set; }
 
-        public ViewModelClass()
+        protected virtual void RaisePropertyChanged([CallerMemberName] string propertyName = null)
         {
-
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
